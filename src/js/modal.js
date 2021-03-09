5432a42b42c1e22d01006eb1bd8f384b144c4b85
Andrii Krychkivskyi <kruchandriy@gmail.com>
2021-03-09 20:19:00 +0200

    task finished

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,28 +1,14 @@
 "use strict"
-console.log('Hello');
 
 const btnOpenModalRef = document.querySelector(".js-open-modal")
 const btnCloseModalRef = document.querySelector(".js-btn-close")
 const modalRef = document.querySelector('.js-modal')
-// const qqq = document.querySelector(".Modal__content")
-
-
-
-console.log(btnOpenModalRef);
-console.log(btnCloseModalRef);
-console.log(modalRef);
-
-const openModal = (event) => {
-    modalRef.classList.add('show-modal')
-}
 
 const toggleModal = (event) => {
     modalRef.classList.toggle('show-modal')
 }
 
 
-
-// console.log("end");
 btnOpenModalRef.addEventListener('click', toggleModal)
 btnCloseModalRef.addEventListener('click', toggleModal)
 modalRef.addEventListener("click", (event) => {
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,27 +1,20 @@
 "use strict"
 
-const sliderRef = document.querySelector('.slider') // основный элемент блока
-const sliderWrapper = sliderRef.querySelector('.slider__wrapper') // обертка для .slider-item
-const sliderItems = sliderRef.querySelectorAll('.slider__item') // элементы (.slider-item)
-// _sliderControls = _mainElement.querySelectorAll('.slider__control'), // элементы управления
- const sliderControlLeft = sliderRef.querySelector('.slider__control_left') // кнопка "LEFT"
- const sliderControlRight = sliderRef.querySelector('.slider__control_right') // кнопка "RIGHT"
-const wrapperWidth = parseFloat(getComputedStyle(sliderWrapper).width) // ширина обёртки
-const itemWidth = parseFloat(getComputedStyle(sliderItems[0]).width) // ширина одного элемента
-let positionLeftItem = 0 // позиция левого активного элемента
- let transform = 0 // значение трансформации .slider_wrapper
-let step = itemWidth / wrapperWidth * 100 // величина шага (для трансформации)
-
+const sliderRef = document.querySelector('.slider')
+const sliderWrapper = sliderRef.querySelector('.slider__wrapper') 
+const sliderItems = sliderRef.querySelectorAll('.slider__item')
+const sliderControlLeft = sliderRef.querySelector('.slider__control_left')
+const sliderControlRight = sliderRef.querySelector('.slider__control_right')
+const wrapperWidth = parseFloat(getComputedStyle(sliderWrapper).width)
+const itemWidth = parseFloat(getComputedStyle(sliderItems[0]).width)
+let positionLeftItem = 0 
+ let transform = 0 
+let step = itemWidth / wrapperWidth * 100
 const items = [];
 
-// наполнение массива элементами .slider__item
 sliderItems.forEach(function (item, index) {
   items.push({ item: item, position: index, transform: 0 });
 });
-
-console.log(items);
-// console.log(itemWidth);
-// console.log( wrapperWidth);
 
 const position = {
     getItemMin: function () {
@@ -31,7 +24,6 @@ const position = {
           indexItem = index;
         }
       });
-      console.log( "getItemMin",indexItem);
       return indexItem;
     },
     getItemMax: function () {
@@ -41,15 +33,12 @@ const position = {
             indexItem = index;
           }
         });
-        console.log( "getItemMax",indexItem);
       return indexItem;
     },
     getMin: function () {
-        console.log("getMin", items[position.getItemMin()].position);
       return items[position.getItemMin()].position;
     },
     getMax: function () {
-        console.log("getMax", items[position.getItemMin()].position);
       return items[position.getItemMax()].position;
     }
   }
@@ -58,7 +47,6 @@ const position = {
     let nextItem;
     if (direction === 'right') {
       positionLeftItem++;
-      console.log("fff", positionLeftItem + wrapperWidth / itemWidth - 1);
       if ((positionLeftItem + wrapperWidth / itemWidth - 1) > position.getMax()) {
         nextItem = position.getItemMin();
         items[nextItem].position = position.getMax() + 1;
@@ -77,20 +65,16 @@ const position = {
       }
       transform += step;
     }
-    console.log(items);
     sliderWrapper.style.transform = 'translateX(' + transform + '%)';
   }
 
   const cycle = function (direction) {
-    // if (!config.isCycling) {
-    //   return;
-    // }
      setInterval(function () {
       transformItem(direction);
-    }, 1000);
+    }, 4000);
   }
 
-//   cycle('right')
+  cycle('right')
 
 sliderControlLeft.addEventListener('click', (e) =>  {
     e.preventDefault()
